36e06de0a762b6c77095b06606d8d68c028b545f
nannapravalika <nannapravalika566@gmail.com>
2024-03-13 13:37:54 +0530

    commit4

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://github.com/nannapravalika/nannapravalika_portfolio/blob/main/src/assets/Pravalika_nanna_CV.pdf`
+const resumeLink = `https://nannapravalika-portfolio.vercel.app/static/media/Pravalika_nanna_CV.ec8b1dcda07dfe1837d5.pdf`
 
 
 const Resume = () => {
